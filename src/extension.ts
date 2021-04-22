0764f4837dc53673c76e8920d8884ee008af4c40
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-04-22 15:25:46 -0300

    Formatting paragraphs.

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,6 @@
 import * as vscode from 'vscode';
 import * as util from './util';
 
-/**
- * Splits the selected lines into words.
- */
-function getWords(document: vscode.TextDocument, idxFirstLine: number, idxLastLine: number): string[] {
-	let words: string[] = [];
-	for (let i = idxFirstLine; i <= idxLastLine; ++i) {
-		const line = document.lineAt(i).text.trimLeft();
-		if (!line.startsWith('//')) {
-			vscode.window.showErrorMessage(`Line ${i + 1} is not a comment.`);
-			return [];
-		}
-
-		words = words.concat(line.split(/\s+/).map((word, idx) => {
-			if (idx === 0) {
-				const commPrefix = util.commentSlashes(word);
-				const wordNoPrefix = word.substr(commPrefix.length);
-				return wordNoPrefix;
-			}
-			return word;
-		}).filter(word => word.length > 0));
-	}
-	return words;
-}
-
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('comment-formatter.formatComment', () => {
 		const editor = vscode.window.activeTextEditor;
@@ -32,43 +8,23 @@ export function activate(context: vscode.ExtensionContext) {
 			const document = editor.document;
 			const idxFirstLine = editor.selection.start.line;
 			const idxLastLine = editor.selection.end.line;
-			const words = getWords(document, idxFirstLine, idxLastLine);
 			const linePrefix = util.linePrefix(document.lineAt(idxFirstLine).text);
-			const eol = util.eol(document);
+			const paragraphs = getParagraphs(document, idxFirstLine, idxLastLine);
 			const maxLen = 80;
 
 			let finalLines: string[] = [];
-			let i = 0;
-			while (i < words.length) {
-				let curLine = linePrefix;
-				let k = i;
-				for (;;) {
-					curLine += ' ' + words[k];
-					const renderLen = curLine.replace(/\t/g, ' '.repeat(4)).length;
-					if (k >= words.length || renderLen > maxLen) break;
-					++k;
+			for (let i = 0; i < paragraphs.length; ++i) {
+				finalLines = finalLines.concat(assemblyFinalParagraph(editor, paragraphs[i], maxLen));
+				if (i < paragraphs.length - 1) {
+					finalLines.push(linePrefix);
 				}
-
-				let numWords = k - i + 1;
-				if (numWords === 1) {
-					finalLines.push(curLine);
-				} else {
-					--numWords;
-					curLine = linePrefix;
-					for (let n = 0; n < numWords; ++n) {
-						curLine += ' ' + words[i + n];
-					}
-					finalLines.push(curLine);
-				}
-
-				i += numWords;
 			}
 
 			let targetSel = new vscode.Selection(
 				new vscode.Position(idxFirstLine, 0),
 				new vscode.Position(idxLastLine, document.lineAt(idxLastLine).text.length),
 			);
-			editor.edit(b => b.replace(targetSel, finalLines.join(eol)));
+			editor.edit(b => b.replace(targetSel, finalLines.join(util.eol(document))));
 
 			editor.selection = new vscode.Selection(
 				new vscode.Position(idxFirstLine, finalLines[0].indexOf('//')),
@@ -82,3 +38,75 @@ export function activate(context: vscode.ExtensionContext) {
 };
 
 export function deactivate() {};
+
+/**
+ * Splits the selected lines into paragraphs, each one containing words.
+ */
+ function getParagraphs(doc: vscode.TextDocument, idxFirstLine: number, idxLastLine: number): string[][] {
+	let paragraphs: string[][] = [];
+	let currentParagraph: string[] = [];
+
+	for (let i = idxFirstLine; i <= idxLastLine; ++i) {
+		const line = doc.lineAt(i).text.trimLeft();
+		if (!line.startsWith('//')) {
+			vscode.window.showErrorMessage(`Line ${i + 1} is not a comment.`);
+			return [];
+		}
+
+		const words = line.split(/\s+/).map((word, idx) => {
+			if (idx === 0) {
+				const commPrefix = util.commentSlashes(word);
+				const wordNoPrefix = word.substr(commPrefix.length);
+				return wordNoPrefix;
+			}
+			return word;
+		}).filter(word => word.length > 0);
+
+		if (words.length === 0) {
+			paragraphs.push(currentParagraph);
+			currentParagraph = [];
+		}
+
+		currentParagraph = currentParagraph.concat(words);
+	}
+
+	paragraphs.push(currentParagraph);
+	return paragraphs.filter(p => p.length > 0);
+}
+
+/**
+ * From an array of words, returns the lines of a formatted comment block.
+ */
+function assemblyFinalParagraph(editor: vscode.TextEditor, words: string[], maxLen: number): string[] {
+	const document = editor.document;
+	const idxFirstLine = editor.selection.start.line;
+	const linePrefix = util.linePrefix(document.lineAt(idxFirstLine).text);
+
+	let finalLines: string[] = [];
+	let i = 0;
+	while (i < words.length) {
+		let curLine = linePrefix;
+		let k = i;
+		for (;;) {
+			curLine += ' ' + words[k];
+			const renderLen = curLine.replace(/\t/g, ' '.repeat(4)).length;
+			if (k >= words.length || renderLen > maxLen) break;
+			++k;
+		}
+
+		let numWords = k - i + 1;
+		if (numWords === 1) {
+			finalLines.push(curLine);
+		} else {
+			--numWords;
+			curLine = linePrefix;
+			for (let n = 0; n < numWords; ++n) {
+				curLine += ' ' + words[i + n];
+			}
+			finalLines.push(curLine);
+		}
+
+		i += numWords;
+	}
+	return finalLines;
+}
